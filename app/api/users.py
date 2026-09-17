e594ad46adaf821122aa2b774b276c82af6eee51
Harshita Baghel <baghelharshita2005@gmail.com>
2026-09-17 14:16:59 +0530

    test: introduce architecture layer violation

--- app/api/users.py
+++ app/api/users.py
@@ -28,3 +28,7 @@ def get_user(user_id: int, service: UserService = Depends(get_user_service)) -> 
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
+
+    def clear(self) -> None:
+        self._users.clear()
+        self._next_id = 1
